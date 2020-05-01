4ebb638fd856c695baf6bd4f6ab36fa01430be9c
Alvaro <6666alvaro666@gmail.com>
2020-04-30 22:42:05 -0500

    insert in html the budget

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //variables
 const budgetUser = prompt("Cual es tu presupuesto Semanal");
 let QuantityBudget;
-//class
+//class budget manage all relaship for logic
 class Budge {
   constructor() {
     this.budgetUser = Number(budgetUser);
@@ -13,11 +13,24 @@ class Budge {
   }
 }
 
+//class interfase manage all relaship for HTML
+class Interfase {
+  insertBudget(quantity) {
+    const budgetUi = document.querySelector("span#total");
+    const remainingUi = document.querySelector("span#restante");
+    //insertHtml
+    budgetUi.innerHTML = `${quantity}`;
+    remainingUi.innerHTML = `${quantity}`;
+  }
+}
 //EventListener
 document.addEventListener("DOMContentLoaded", function () {
   if (budgetUser == null || budgetUser === "") {
     window.location.reload();
   } else {
+    //create obj to budget
     QuantityBudget = new Budge(budgetUser);
+    const ui = new Interfase();
+    ui.insertBudget(QuantityBudget.budgetUser);
   }
 });
