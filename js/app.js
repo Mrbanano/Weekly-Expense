cb5f57de52f9204d70f7ef2ca0811ec94f0cbc5f
Alvaro <6666alvaro666@gmail.com>
2020-05-01 21:08:24 -0500

    add slider function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,97 @@
 //variables
-const budgetUser = prompt("Cual es tu presupuesto Semanal");
+//const budgetUser = prompt("Cual es tu presupuesto Semanal");
 const form = document.getElementById("agregar-gasto");
 let QuantityBudget;
+
+//cont elements
+class IndexForSiblings {
+  static get(el) {
+    let children = el.parentNode.children;
+
+    for (let i = 0; i < children.length; i++) {
+      let child = children[i];
+      if (child == el) return i;
+    }
+  }
+}
+//control slider
+class Slider {
+  constructor(selector, movimiento = true) {
+    this.move = this.move.bind(this);
+    this.moveByButton = this.moveByButton.bind(this);
+    this.slider = document.querySelector(selector);
+    this.itemsCount = this.slider.querySelectorAll(".Daycontainer >* ").length;
+    this.interval = null;
+    this.contador = 0;
+    this.movimiento = movimiento;
+
+    this.start();
+    this.buildControls();
+    this.bindEvents();
+  }
+
+  start() {
+    if (!this.movimiento) return;
+    this.interval = window.setInterval(this.move, 5000);
+  }
+  resetContador() {
+    if (this.interval) window.clearInterval(this.interval);
+    this.start();
+  }
+  bindEvents() {
+    this.slider.querySelectorAll(".controls li").forEach((item) => {
+      item.addEventListener("click", this.moveByButton);
+    });
+  }
+  moveByButton(ev) {
+    let index = IndexForSiblings.get(ev.currentTarget);
+    this.contador = index;
+    this.moveTo(index);
+    this.resetContador();
+  }
+  move() {
+    this.contador++;
+    if (this.contador > this.itemsCount - 1) this.contador = 0;
+    this.moveTo(this.contador);
+  }
+
+  resetIndicador() {
+    this.slider
+      .querySelectorAll(".controls li.active")
+      .forEach((item) => item.classList.remove("active"));
+  }
+  moveTo(index) {
+    const card = -1 * document.querySelector("#dayOne").clientWidth;
+    let left = index * card;
+    let indicador = index + 1;
+    this.resetIndicador();
+    this.slider
+      .querySelector(".controls li:nth-child(" + indicador + ")")
+      .classList.add("active");
+    this.slider.querySelector(".Daycontainer").style.left = left + "px";
+  }
+
+  buildControls() {
+    const Day = ["D", "L", "M", "M", "J", "V", "S"];
+    let InitalDay = Day[3];
+    let InitalDaty = 5;
+    for (let i = 0; i < this.itemsCount; i++) {
+      let control = document.createElement("li");
+
+      control.innerHTML = `
+        <div class="circuleDay">
+        <span class="sub"><strong>${InitalDaty + i}</strong></span>
+        <span class="sub">${Day[i]}</span>
+        </div>
+      `;
+
+      if (i == 0) control.classList.add("active");
+
+      this.slider.querySelector(".controls ul").appendChild(control);
+    }
+  }
+}
+
 //class budget manage all relaship for logic
 class Budge {
   constructor() {
@@ -84,14 +174,16 @@ class Interfase {
 }
 //EventListener
 document.addEventListener("DOMContentLoaded", function () {
-  if (budgetUser == null || budgetUser === "") {
-    window.location.reload();
-  } else {
-    //create obj to budget
-    QuantityBudget = new Budge(budgetUser);
-    const ui = new Interfase();
-    ui.insertBudget(QuantityBudget.budgetUser);
-  }
+  new Slider(".slider", true);
+
+  //if (budgetUser == null || budgetUser === "") {
+  //window.location.reload();
+  //} else {
+  //create obj to budget
+  //  QuantityBudget = new Budge(budgetUser);
+  //const ui = new Interfase();
+  // ui.insertBudget(QuantityBudget.budgetUser);
+  // }
 });
 form.addEventListener("submit", function (e) {
   e.preventDefault();
