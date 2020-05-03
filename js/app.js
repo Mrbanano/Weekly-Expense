422b92ba2ff5de69a1ca8694cf496d0507b479ad
Alvaro <6666alvaro666@gmail.com>
2020-05-02 19:38:27 -0500

    fix dynamic date

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 //variables
-//const budgetUser = prompt("Cual es tu presupuesto Semanal");
+let budgetUser = 0;
+let accumulatedExpense = 0;
+let dayLetter;
+let yearInitial = 0;
+let monthInitial = 0;
+let dayInitial = 0;
 //const form = document.getElementById("agregar-gasto");
 const btnMode = document.getElementById("switch");
 const btnAdd = document.getElementById("add");
@@ -74,22 +79,61 @@ class Slider {
   }
 
   buildControls() {
-    const Day = ["D", "L", "M", "M", "J", "V", "S"];
-    let InitalDay = Day[3];
-    let InitalDaty = 5;
+    let InitalDaty = Number(dayInitial);
+    let day = this.getDaysLetter(dayLetter);
+    let limitMonth = this.getDaysLimit(monthInitial, yearInitial);
+
     for (let i = 0; i < this.itemsCount; i++) {
       let control = document.createElement("li");
-
       control.innerHTML = `
         <div class="circuleDay">
         <span class="sub"><strong>${InitalDaty + i}</strong></span>
-        <span class="sub">${Day[i]}</span>
+        <span class="sub">${day[i]}</span>
         </div>
       `;
-
       if (i == 0) control.classList.add("active");
 
       this.slider.querySelector(".controls ul").appendChild(control);
+    }
+  }
+
+  getDaysLetter(dayInitial) {
+    let Day = [];
+    switch (dayInitial) {
+      case 0:
+        Day = ["D", "L", "M", "M", "J", "V", "S"];
+        break;
+      case 1:
+        Day = ["L", "M", "M", "J", "V", "S", "D"];
+        break;
+      case 2:
+        Day = ["M", "M", "J", "V", "S", "D", "L"];
+        break;
+      case 3:
+        Day = ["M", "J", "V", "S", "D", "L", "M"];
+        break;
+      case 4:
+        Day = ["J", "V", "S", "D", "L", "M", "M"];
+        break;
+      case 5:
+        Day = ["V", "S", "D", "L", "M", "M", "J"];
+        break;
+      case 6:
+        Day = ["S", "D", "L", "M", "M", "J", "V"];
+        break;
+    }
+    return Day;
+  }
+
+  getDaysLimit(month, year) {
+    //check if year is bisiesto
+    const yearbase = 2020;
+    let isBisisto = false;
+    for (let i = 0; i < 21; i++) {
+      let a = yearbase + i * 4;
+      if (a === Number(year)) {
+        isBisisto = true;
+      }
     }
   }
 }
@@ -99,21 +143,27 @@ class Budge {
   constructor() {
     this.budgetUser = Number(budgetUser);
     this.remaining = Number(budgetUser);
+    this.expenditure = Number(accumulatedExpense);
   }
-
   bundgetRemaining(quantity = 0) {
+    this.BundgetExpense(quantity);
     return (this.remaining -= Number(quantity));
   }
+  BundgetExpense(expenditure = 0) {
+    return (this.accumulatedExpense += expenditure);
+  }
 }
-
 //class interfase manage all relaship for HTML
 class Interfase {
   insertBudget(quantity) {
     const budgetUi = document.querySelector("span#total");
     const remainingUi = document.querySelector("span#restante");
+    const accumulatedExpenses = document.querySelector("span#Expense");
     //insertHtml
-    budgetUi.innerHTML = `${quantity}`;
-    remainingUi.innerHTML = `${quantity}`;
+    quantity = quantity.toFixed(2);
+    budgetUi.innerHTML = `$${quantity}`;
+    remainingUi.innerHTML = `$${quantity}`;
+    accumulatedExpenses.innerHTML = `$${accumulatedExpense}`;
   }
   printMessage(message, type) {
     const divMessage = document.createElement("div");
@@ -176,10 +226,17 @@ class Interfase {
 }
 //EventListener
 document.addEventListener("DOMContentLoaded", function () {
+  const year = dayjs().format("YYYY");
+  const month = dayjs().format("M");
+  const day = dayjs().format("D");
+  const dateletter = new Date().getDay();
+  yearInitial = year;
+  monthInitial = month;
+  dayLetter = dateletter;
+  dayInitial = day;
   new Slider(".slider", false);
-
-  (async () => {
-    const { value: budget } = await Swal.fire({
+  /*(async () => {
+    const { value: budgetInitail } = await Swal.fire({
       imageUrl: "./img/money.svg",
       imageHeight: 100,
       imageAlt: "Administra tus gastos",
@@ -192,23 +249,77 @@ document.addEventListener("DOMContentLoaded", function () {
       inputPlaceholder: " $00.00Mxn",
       inputValue: "",
     });
-    console.log(budget);
-    if (budget == null || budget === "") {
+    budgetUser = budgetInitail;
+    if (budgetUser == null || budgetUser === "") {
       window.location.reload();
     } else {
-      //create obj to budget
-      QuantityBudget = new Budge(budget);
-      const ui = new Interfase();
+      QuantityBudget = new Budge(budgetUser);
       console.log(QuantityBudget);
-      //ui.insertBudget(budgetUser);
+      const ui = new Interfase();
+      ui.insertBudget(QuantityBudget.budgetUser);
     }
-  })();
+  })();*/
 });
+
 btnMode.addEventListener("click", function (e) {
   e.preventDefault();
   btnMode.classList.toggle("active");
 });
 
 btnAdd.addEventListener("click", function (e) {
-  Swal.fire({});
+  (async () => {
+    const { value: formValues } = await Swal.fire({
+      imageUrl: "./img/outsale.svg",
+      imageHeight: 100,
+      imageAlt: "Agrega un gasto",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      stopKeydownPropagation: false,
+      title: "¡Gasto!",
+      text: "Agrega un gasto",
+      html:
+        '<input id="swal-input1" required placeholder="Nombre del gasto" class="swal2-input text-center">' +
+        '<input id="swal-input2" required placeholder="Monto Gastado $00.00"class="swal2-input text-center">',
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          document.getElementById("swal-input1").value,
+          document.getElementById("swal-input2").value,
+        ];
+      },
+      function(formValues) {
+        const value = Swal.fire(JSON.stringify(formValues));
+        console.log(value);
+        if (value[0] === "" || value[0] === null) {
+          swal.showInputError("Necesitas ponerle Nombre al gasto!");
+          return false;
+        }
+      },
+    });
+    //read data form
+    const nameSpending = formValues[0];
+    const quantitySpending = formValues[0];
+    //check empty
+    if (nameSpending === "" || quantitySpending === "") {
+      return false;
+      console.log("no hay nada ");
+    }
+  })();
 });
+
+/*form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(click);
+
+  //instance interfases
+  //const ui = new Interfase();
+ //check empty inputs
+  //
+  //ui.printMessage("Te hace falta algun campo.", "error");
+  //} else {
+  // ui.printMessage("Gasto agregado", "ok");
+  //ui.addSpending(nameSpending, quantitySpending);
+  // ui.remainingBudget(quantitySpending);
+  //}
+});
+*/
